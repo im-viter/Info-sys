370bbb7dc86abb738143890251d34d0f0ff9a8ed
Exin <viter5647@gmail.com>
2025-07-27 21:41:38 +0330

    modify the labled file

--- label.py
+++ label.py
@@ -4,9 +4,9 @@ import pyfiglet
 from termcolor import colored
 
 ascii_art = pyfiglet.figlet_format("Viter", font="basic")
-# text = pyfiglet.figlet_format("Society", font="linux")
+
 colored_art = colored(ascii_art, color="red")
-# color = colored(text, color="magenta")
+
 
 print(colored_art)
-# print(color)
+
